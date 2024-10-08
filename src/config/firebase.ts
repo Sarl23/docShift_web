47bed3f8b96c13aba815c2bbceca5be87acf87e3
Sarl23 <sergioandresrojasleon@gmail.com>
2024-10-08 00:09:25 -0500

    fix: :fire: Implement env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyCgzLj4Ft7efgc2IgxA1LFuRsk_soc3XCY",
-    authDomain: "mediapp-93c21.firebaseapp.com",
-    projectId: "mediapp-93c21",
-    storageBucket: "mediapp-93c21.appspot.com",
-    messagingSenderId: "715287550884",
-    appId: "1:715287550884:web:ae88d3daa429440e497f31",
-    measurementId: "G-L313QL1WQH"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
