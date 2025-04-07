68d2ed4a23c0001c53ddd8b756e4b78491d95a5a
Sarl23 <sergioandresrojasleon@gmail.com>
2025-04-06 21:37:32 -0500

    refactor: :fire: remove old way to login with googleAuth

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { AuthProvider, useFirebaseApp } from "reactfire";
-import { getAuth } from "firebase/auth";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./config/router";
 
 const App = () => {
-  const app = useFirebaseApp();
-  const auth = getAuth(app);
 
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <AuthProvider sdk={auth}>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 };
